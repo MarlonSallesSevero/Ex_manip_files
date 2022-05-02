ca68047a64f6817f5b60df865403866c3014d60a
Marlon Salles <marlonsalles@hotmail.com>
2022-05-01 22:23:22 -0300

    Exemplo Manipulação de Arquivos com C#

---
+++ EX/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace EX.Entities
+{
+    internal class Product
+    {
+        public string nome { get; set; }
+        public double preco { get; set; }
+        public int qtd { get; set; }
+
+        public Product(string nome, double preco, int qtd)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.qtd = qtd;
+        }
+        public double total()
+        {
+            return qtd * preco;
+        }
+    }
+}
---
+++ EX/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using EX.Entities;
+namespace EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path:");
+            string sourceFilePath = Console.ReadLine();
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFilePath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+                Directory.CreateDirectory(targetFolderPath);
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (var item in lines)
+                    {
+                        string[] campos = item.Split(',');
+                        string name = campos[0];
+                        double price = double.Parse(campos[1], CultureInfo.InvariantCulture);
+                        int qtd = int.Parse(campos[3], CultureInfo.InvariantCulture);
+                        Product prod = new Product(name, price, qtd);
+                        sw.WriteLine(prod.nome + "," + prod.total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine($"Erro:\n{ex.Message}");
+            }
+            finally
+            {
+                string a = Console.ReadLine();
+            }
+        }
+    }
+}
